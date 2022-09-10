7f72a2715779cee113c92bca4f8df1a23b2793d9
alexsam29 <69481177+alexsam29@users.noreply.github.com>
2022-09-10 17:14:09 -0400

    Text file contents can be displayed on console.

--- bin/index.js
+++ bin/index.js
@@ -1,13 +1,22 @@
 #!/usr/bin/env node
 const yargs = require("yargs");
 
-const options = yargs.usage("Usage: -n <name>").option("n", {
-  alias: "name",
-  describe: "Your name",
+const options = yargs.usage("Usage: -i <input>").option("i", {
+  alias: "input",
+  describe: "File name",
   type: "string",
   demandOption: true,
 }).argv;
 
-const output = `Hello, ${options.name}!`;
+const output = `This is the file name you chose, ${options.input}!`;
 
 console.log(output);
+
+const fs = require("fs");
+fs.readFile(options.input, (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data.toString());
+});
